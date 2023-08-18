ebec481122f7018fa8612a91b3d49101eb7c4069
Herliana Nur Ekawati <herliananur19@gmail.com>
2023-08-18 15:11:40 +0800

    Menambahkan program latihan

---
+++ Minggu 1/3. Fundamental Dart/Latihan/dart_fundamental_1/bin/operator.dart
@@ -0,0 +1,46 @@
+// Aritmatic
+// void main() {
+//   print(1 + 2);
+//   print(4 - 2);
+//   print(2 * 2);
+//   print(10 / 2);
+//   print(5 % 2);
+// }
+
+// Assignment
+// void main() {
+//   var usia = 18.0;
+//   usia = 19;
+//   print(usia);
+//   usia += 2;
+//   print(usia);
+//   usia -= 2;
+//   print(usia);
+//   usia *= 2;
+//   print(usia);
+//   usia /= 2;
+//   print(usia);
+//   usia %= 2;
+//   print(usia);
+// }
+
+// Comparison
+// void main() {
+//   print(1 == 2);
+//   print(1 < 2);
+//   print(1 <= 2);
+//   print(1 > 2);
+//   print(1 >= 2);
+// }
+
+// Logical
+void main() {
+  print(1 == 2 && 2 == 1);
+  print(2 == 2 && 2 == 2);
+  print(2 == 1 && 2 == 1);
+  print(2 == 1 || 2 == 2);
+  print(2 == 1 || 2 == 1);
+
+  bool notTrue = !true;
+  print(notTrue);
+}
