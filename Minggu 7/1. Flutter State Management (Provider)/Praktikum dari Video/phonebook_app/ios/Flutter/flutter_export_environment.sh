d052a75c8c5f512bd5d6ffc3812be66c1509fffa
Herliana Nur Ekawati <herliananur19@gmail.com>
2023-10-01 15:22:44 +0800

    Menambahkan latihan dari video

---
+++ Minggu 7/1. Flutter State Management (Provider)/Praktikum dari Video/phonebook_app/ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=C:\Development\flutter"
+export "FLUTTER_APPLICATION_PATH=E:\ALTERRA ACADEMY\GitHub\Flutter_Herliana-Nur-Ekawati\Minggu 7\1. Flutter State Management (Provider)\Praktikum dari Video\phonebook_app"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_TARGET=lib\main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=true"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=.dart_tool/package_config.json"
